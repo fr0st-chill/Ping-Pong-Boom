5bc72407a03cac9ae084e70ff0dfd46ea5b67444
Summer Cordaella <fr0st-chill@pm.me>
2026-06-15 13:37:50 -0400

    Project repo init

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <SDL2/SDL.h>
+
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
+
+int main() {
+  // Init SDL Graphic Components to Main Function
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window* window = SDL_CreateWindow("Pong", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+  // Main Game Logic
+  {
+    bool running = true;
+
+    // Loop and process events until user exits
+    while (running)
+    {
+      SDL_Event event;
+      while (SDL_PollEvent(&event))
+      {
+        if (event.type == SDL_QUIT)
+        {
+          running =  false;
+        }
+        else if (event.type == SDL_KEYDOWN)
+        {
+          if (event.key.keysym.sym == SDLK_ESCAPE)
+          {
+            running = false;
+          }
+        }
+      }
+      // Clear to Black
+      SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0xFF);
+      SDL_RenderClear(renderer);
+      // Rendering
+      // Present the backbuffer
+      SDL_RenderPresent(renderer);
+    }
+  }
+
+  // Clean-up the Runtime Environment
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
